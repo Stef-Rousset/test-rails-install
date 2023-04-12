3d4e15ca12654fd83f72682bdda23b00b636cbb6
stephanie rousset <strabon@club-internet.fr>
2023-04-12 16:25:46 +0200

    testing models

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -4,4 +4,8 @@ class ShopsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get root url" do
+    get root_path
+    assert_response :success
+  end
 end
