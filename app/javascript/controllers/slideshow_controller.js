f388252db68a6d1aeca6b64fda58fb137fb9d925
stephanie rousset <strabon@club-internet.fr>
2023-04-24 17:01:12 +0200

    finish with stimulus

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -1,14 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["slide"] //crée la propriété pr la cible slide this.slideTarget
+  static targets = ["slide"] //crée la propriété pr la cible slide this.slideTarget si une slide, sinon this.slideTargets
 
   // initialize is invoked once when controller is instantiated
   // connect is invoked anytime when the controller is connected to the DOM
   // disconnect is invoked anytime when the controller is disconnected from the DOM
   initialize() {
+      console.log(this.element) //this.element correspond à la div qui porte le data-controller
       this.index = 0
       this.showCurrentSlide()
+
     }
     next(){
       if (this.index < 2){
